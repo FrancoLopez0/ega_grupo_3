2831d7d9d882cb26f5cfcf461949881618844c66
FrancoLopez0 <francoalelopez@gmail.com>
2025-06-22 18:54:30 -0300

    part 1

--- 3_trabajos_practicos/tp4/firmware/firmware.c
+++ 3_trabajos_practicos/tp4/firmware/firmware.c
@@ -6,14 +6,14 @@
 #include "semphr.h"
 #include "queue.h"
 
-#define SDA_PIN 4U
-#define SCL_PIN 5U
-#define LCD_DIR 0x27 
-#define I2C_FREQ 100000
+#define SDA_PIN          4U
+#define SCL_PIN          5U
+#define LCD_DIR          0x27 
+#define I2C_FREQ         100000
 
-#define BUFF_SIZE_LCD 10
-#define BUFF_SIZE_I2C 10
-#define SAMPLE_TIME   1000
+#define BUFF_SIZE_LCD    10
+#define BUFF_SIZE_I2C    10
+#define SAMPLE_TIME_MS   1000
 
 typedef struct 
 {
@@ -22,9 +22,15 @@ typedef struct
     uint8_t pos;
 }lcd_w_t;
 
+typedef struct 
+{
+    float temp;
+    float pres;
+}bmp_t;
+
 SemaphoreHandle_t g_sI2c;
 
-QueueHandle_t g_qLcd, g_qI2C;
+QueueHandle_t g_qLcd;
 
 void i2c_config(void){
     i2c_init(i2c0, I2C_FREQ);
@@ -33,8 +39,6 @@ void i2c_config(void){
 
     gpio_pull_up(SDA_PIN);
     gpio_pull_up(SCL_PIN);
-
-    bi_decl(bi_2pins_with_func(SDA_PIN,SCL_PIN, GPIO_FUNC_I2C));
 }
 
 void lcd_guardian_task(void *args){
@@ -47,11 +51,9 @@ void lcd_guardian_task(void *args){
     lcd_set_cursor(1,0);
     lcd_string("Pres:");
     
-
     while(1){
         if(xQueueReceive(g_qLcd, &w, portMAX_DELAY)){
             xSemaphoreTake(g_sI2c, portMAX_DELAY);
-                printf("%s, (%d,%d)\n", w.str, w.line, w.pos);
                 lcd_set_cursor(w.line, w.pos);
                 lcd_string(w.str);
             xSemaphoreGive(g_sI2c);
@@ -61,31 +63,44 @@ void lcd_guardian_task(void *args){
 }
 
 void bmp_sense_task(void *args){
-    lcd_w_t bmp_temp = {
+    lcd_w_t lcd_write = {
         .str="--",
         .line = 0,
-        .pos = 6
+        .pos = 6,
     };
-    lcd_w_t bmp_pres = {
-        .str="--",
-        .line = 1,
-        .pos = 6
+
+    bmp_t measures = {
+        .pres = 0.0f,
+        .temp = 0.0f
     };
-    
-    uint32_t c = 0;
+
+    int32_t raw_temp, raw_preasure;
+
+    struct bmp280_calib_param calib_param;
+
+    bmp280_get_calib_params(&calib_param);
+
+    uint32_t c=0;
 
     while (1)
     {
         xSemaphoreTake(g_sI2c, portMAX_DELAY);
-            printf("Sensar BMP\n");
-            sprintf(bmp_temp.str, "%d %cC", c, (char)223);
-            sprintf(bmp_pres.str, "%d Kpa", c);
+            bmp280_read_raw(&raw_temp,&raw_preasure);
         xSemaphoreGive(g_sI2c);
 
-        xQueueSend(g_qLcd, &bmp_temp, portMAX_DELAY);
-        xQueueSend(g_qLcd, &bmp_pres, portMAX_DELAY);
-        c++;
-        vTaskDelay(pdMS_TO_TICKS(SAMPLE_TIME));
+        measures.pres = bmp280_convert_pressure(raw_preasure, raw_temp, &calib_param) / 1000;
+        measures.temp = bmp280_convert_temp(raw_temp, &calib_param);
+
+        lcd_write.line = 0;
+        sprintf(lcd_write.str, "%.2f %cC", measures.temp, (char)223);
+        xQueueSend(g_qLcd, &lcd_write, portMAX_DELAY);
+
+        lcd_write.line = 1;
+        sprintf(lcd_write.str, "%.2f Kpa", measures.pres);
+        xQueueSend(g_qLcd, &lcd_write, portMAX_DELAY);
+        printf(lcd_write.str);
+
+        vTaskDelay(pdMS_TO_TICKS(SAMPLE_TIME_MS));
     }    
 }
 
@@ -94,12 +109,12 @@ int main()
     stdio_init_all();
 
     g_qLcd = xQueueCreate(BUFF_SIZE_LCD, sizeof(lcd_w_t));
-    // xQueueCreate(BUFF_SIZE_I2C, sizeof(void*));
 
     g_sI2c = xSemaphoreCreateMutex();
 
     i2c_config();
     lcd_init(i2c0, LCD_DIR);
+    bmp280_init(i2c0);
 
     xTaskCreate(
         lcd_guardian_task,
