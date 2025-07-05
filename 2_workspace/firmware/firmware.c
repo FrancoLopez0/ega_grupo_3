b3a5de6f5a8df43c584078b0969a4dbec88f3ad8
FrancoLopez0 <francoalelopez@gmail.com>
2025-07-05 18:35:25 -0300

    init ui

--- 2_workspace/firmware/firmware.c
+++ 2_workspace/firmware/firmware.c
@@ -14,12 +14,18 @@
 
 #include "modules/temt6000/temt6000.h"
 #include "modules/bh1750/bh1750.h"
+#include "modules/ui/ui.h"
 
-#define I2C_FREQ 400*1000
+#define I2C_FREQ        400*1000
+#define PIN_TEMT6000    28
+#define CHANN_TEMT6000  2
 
-#define PIN_TEMT6000 28
-#define CHANN_TEMT6000 2
-
+user_t user = {
+    .sp = MAX_SP/2,
+    .mode = true,
+    .lux = MAX_LUX/2,
+    .select = set_sp
+};
 
 void i2c_config(void){
     i2c_init(I2C_PORT, I2C_FREQ);
@@ -28,7 +34,6 @@ void i2c_config(void){
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-
 }
 
 void adc_config(void){
@@ -42,12 +47,27 @@ int main() {
     adc_init();
     i2c_config();
     bh1750_init();
+
+    ssd1306_t oled;
+    oled.external_vcc = false;
+    ui_init(&oled, I2C_PORT, &user);
+
+
+    uint8_t c = 0;
     
     while (1) {
-        printf("Lux TEMT6000:%f\n", temt6000_get_lux());
-        uint16_t lux = bh1750_read_lux();
-        printf("Luz BH1750: %u lux\n", lux);
-        sleep_ms(500);
+        // printf("Lux TEMT6000:%f\n", temt6000_get_lux());
+        // uint16_t lux = bh1750_read_lux();
+        // printf("Luz BH1750: %u lux\n", lux);
+
+        user.sp = (user.sp==0) ? MAX_SP:(user.sp - 1);
+
+        ui_update(&oled, &user);
+        
+        user.select = (c==20) ? (user.select + 1)%(not_show) : user.select;
+        c = (c+1)%21;
+
+        sleep_ms(50);
     }
 }
 
